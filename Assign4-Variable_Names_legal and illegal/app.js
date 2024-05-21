4ac73b13be7728aaff7d44e23ae602334813b469
Muhammad Subhan Khan <148111447+MuhammadSubhan0712@users.noreply.github.com>
2024-05-22 00:20:20 +0500

    Add files via upload

---
+++ Assign4-Variable_Names_legal and illegal/app.js
@@ -0,0 +1,35 @@
+// --------------------------------------------------------------
+// Task # 01:
+// 1. Declare 3 variables in one statement.
+var named , fathers_name , age ;
+
+
+// --------------------------------------------------------------
+// Task # 02:
+//2. Declare 5 legal & 5 illegal variable names.
+// legal variable names are:
+var myname; //without space b/w letters example
+var _myname; // underscore example
+var $myname; //Dollar case Example
+var MyName ; //camel Case Example
+var my_name; // underscore example
+
+// Illegal variable names are:
+var 1stname; //starts with digit
+var my name; //contain space
+var break; //using reserved key
+var my#name ; //using # b/w letters
+var my-name; //using dash b/w letters
+
+
+// --------------------------------------------------------------
+// Task # 03:
+// 3. Display this in your browser
+// a. A heading stating “Rules for naming JS variables”
+// b. Variable names can only contain underscore (_),dollar sign ($), 
+// without space and camel case. 
+// For example $my_1stVariable
+// c. Variables must begin with a ______, ______ or 
+// _____. For example $name, _name or name
+// d. Variable names are case sensitive.
+// e. Variable names should not be JS variable.

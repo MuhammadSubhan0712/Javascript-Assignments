d98d776f600e39e4094c2af1e3ce3b56e878a0a9
Muhammad Subhan Khan <148111447+MuhammadSubhan0712@users.noreply.github.com>
2024-05-22 00:19:32 +0500

    Add files via upload

---
+++ Assign1_Alerts/alerts.js
@@ -0,0 +1,27 @@
+// //Task # 01
+// //1.Write a script to greet your website visitor using JS alert box.
+// alert('Welcome to our website');
+// --------------------------------------------------------------------
+// // //Task # 02
+// // //2.Write a script to display following message on your web page:
+// alert('JAVAScript Alert');
+// alert('Error!.Please enter valid password');
+// // --------------------------------------------------------------------
+// //Task # 03
+// //3.Write a script to display following message on your web page: (Hint : Use line break)
+// alert('JAVAScript Alert ');
+// alert('Welcome to JS Land...\nHappy Coding!.');
+// // --------------------------------------------------------------------
+// //Task # 04
+// //4.Write a script to display following messages in sequence:
+// alert('JAVAScript Alert ');
+// alert('Welcome to JS Land...\nHappy Coding!.');
+
+// // --------------------------------------------------------------------
+// //Task # 05
+// //5.Generate the following message through browser’s developer console:
+// alert('JAVAScript Alert ');
+// alert("Hello. I can run JS through my web browser's console");
+
+
+

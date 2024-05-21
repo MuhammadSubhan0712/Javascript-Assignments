c74f43faf195ba3612f4106378ee90e0674b89a9
Muhammad Subhan Khan <148111447+MuhammadSubhan0712@users.noreply.github.com>
2024-05-22 00:19:47 +0500

    Add files via upload

---
+++ Assign2-Variable_for_Strings/variable_names.js
@@ -0,0 +1,84 @@
+// ---------------------------------------------------
+// Task#01
+// 1. Declare a variable called username.
+var username;
+
+// ---------------------------------------------------
+// Task#02
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+var myName = "Muhammad Subhan Khan";
+
+// ---------------------------------------------------
+// Task#03
+// 3. Write a script to
+// a. declare a JS variable, titled message
+// b. assign “Hello World” to the variable message
+// c. display the message in alert box
+var title_message = "Hello World";
+alert(title_message);
+
+// ---------------------------------------------------
+// Task#04
+// 4. Write a script to save student’s bio data in JS variables
+// and show the data in alert boxes. Example:
+alert("JavaScript Alert");
+var names = "Ali Khan ";
+var age  = "  17 years old ";
+var std = "  SSUET ";
+var sub = "  Mobile & Cloud Computing - Module A";
+alert (names + age + std + sub);
+
+// ---------------------------------------------------
+// Task#05
+//5. Write a script to display the following alert using one JS variable:
+alert("JavaScript Alert");
+alert("PIZZA");
+alert("PIZZ");
+alert("PIZ");
+alert("PI");
+alert("P");
+
+// ---------------------------------------------------
+// Task#06
+//6. Fortune Teller: Why pay a fortuneteller when you can just program your fortune yourself?
+// Store the following into variables: number of children,
+// partner’s name, geographic location, and job title.
+alert("JavaScript Alert");
+var job_title = "You will be a Software Developer";
+var geographic_location = " in USA ";
+var partners_name = " and married with ABC ";
+var number_of_children = " with two childrens ";
+alert (job_title + geographic_location + partners_name + number_of_children);
+
+// ---------------------------------------------------
+// Task#07
+//7. Declare a variable called email & assign to it a string that represents your Email Address (e.g. abc@xyz.com).
+// Show the below mentioned message in an alert box. (Hint: use string concatenation)
+alert("JavaScript Alert");
+var email = "muhammadsubhan0712@gmail.com";
+alert("My email address is " +email);
+
+// ---------------------------------------------------
+// Task#08
+//8. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. 
+// Display the following message in an alert box:
+alert("JavaScript Alert");
+var book = "I am trying to learn from a book 'A smarter way to learn JavaScript'";
+alert(book);
+
+
+
+// ---------------------------------------------------
+// Task#09
+// 9. Use the variables, declared in exercise 4 & show the details giving meaning to data. 
+//Like “My name is Ali Khan”, “I am 17 years old”, “I study in SSUET”, “I take Mobile & Cloud 
+// Computing, Module A classes”
+alert("JavaScript Alert");
+var names = "Ali Khan.";
+var age  = "17 years old.";
+var std = "SSUET.";
+var sub = "Mobile & Cloud Computing - Module A classes.";
+alert ("My name is " + names + " I am " + age + " I study in " + std + " I take " + sub);
+
+
